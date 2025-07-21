88a0653bc46b70f73b03fd8eb9f039facaf3c0e7
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-07-21 13:01:05 -0400

    rendered location details from geolocation api

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,27 @@
-
 import { fetchIpGeolocationData } from "./services/geoLocationApiService.js";
+import { Location } from "./model/Location.js";
+import { GeolocationAPI } from "./model/GeolocationAPI.js";
+
+let locationData: Location | null = null;
+let geoLocationData : GeolocationAPI | null = null;
+
+const renderLocationUI = () => {
+  if (!locationData || !geoLocationData) return;
+
+  document.getElementById("ip-address")!.textContent = geoLocationData.ip;
+  document.getElementById("location")!.textContent = locationData.completeAddress;
+  document.getElementById("timezone")!.textContent = `UTC ${locationData.timezone}`;
+  document.getElementById("isp")!.textContent = geoLocationData.ispName;
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    const rawData = await fetchIpGeolocationData(); 
+    const rawData = await fetchIpGeolocationData();
     console.log("Geolocation data:", rawData);
-
-    // Example: access location info
     console.log(rawData.location.city, rawData.ip, rawData.isp);
+    geoLocationData = new GeolocationAPI(rawData);
+    locationData = geoLocationData.location
+    renderLocationUI();
   } catch (err) {
     console.error("Error fetching IP data:", err);
   }
